66f2256d520b643ceb40b8295814cf0fbd5f38f1
Fernanda-Marcano <marcanofernanda17@gmail.com>
2024-10-12 20:28:12 -0400

    Creación de los modelos del proyecto y registro de los modelos en el admin de django terminados

--- apps/library_app/admin.py
+++ apps/library_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Autor, Category, Book
 
-# Register your models here.
+
+class AutorAdmin(admin.ModelAdmin):
+    fields = ['id','name', 'last_name', 'biography']
+    filter_vertical = ['id', 'name', 'last_name']
+    list_display = ['name', 'last_name', 'biography']
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'pub_year', 'autor', 'category']
+    list_filter = ['title', 'autor', 'category', 'pub_year']
+    date_hierarchy = ['pub_year']
+
+admin.site.register(Autor, AutorAdmin)
+
+admin.site.register(Category)
+
+admin.site.register(Book, BookAdmin)
--- apps/library_app/models.py
+++ apps/library_app/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Autor(models.Model):
+    """Model definition for Autor."""
+    # TODO: Define fields here
+    name = models.CharField(verbose_name='Nombre del Autor', max_length=100, null=False, blank=False)
+    last_name = models.CharField(verbose_name='Apellido', max_length=100, null=False, blank=False)
+    biography = models.TextField(verbose_name='Biografía', null=True, blank=True)
+
+    class Meta:
+        """Meta definition for Autor."""
+        db_table = 'Autor'
+        ordering = ['id', 'name']
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        """Unicode representation of Autor."""
+        return f'{self.name} {self.last_name}'
+
+
+class Category(models.Model):
+    """Model definition for Category."""
+    # TODO: Define fields here
+    name = models.CharField(verbose_name='Nombre de Categoría', max_length=100, null=False, blank=False, unique=True)
+
+    class Meta:
+        """Meta definition for Category."""
+        db_table = 'Categoría'
+        ordering = ['name']
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    def __str__(self):
+        """Unicode representation of Category."""
+        return self.name
+
+
+class Book(models.Model):
+    """Model definition for Book."""
+    # TODO: Define fields here
+    title = models.CharField(verbose_name='Título del Libro', max_length=200, null=False, blank=False, unique=True)
+    description = models.TextField(verbose_name='Descripción', null=False, blank=False)
+    pub_year = models.DateField(verbose_name='Fecha de Publicación', null=False, blank=False)
+    image = models.ImageField(verbose_name='Imagen', upload_to='image/', default='image/fondo.png')
+    file = models.FileField(verbose_name='Libro', upload_to='books/', null=True, blank=True)
+    autor = models.ForeignKey('Autor', on_delete=models.RESTRICT, verbose_name='Autor')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, verbose_name='Categoría', null=True)
+
+    class Meta:
+        """Meta definition for Book."""
+        db_table = 'Libro'
+        ordering = ['title']
+        verbose_name = 'Libro'
+        verbose_name_plural = 'Libros'
+
+    def __str__(self):
+        """Unicode representation of Book."""
+        return f'{self.title}'
